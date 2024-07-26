a1fe57de92feb61878cacd01e81699ab652f7345
RozalinaRavi <violet_star@ukr.net>
2024-07-26 12:49:52 +0300

    first commit

---
+++ dev/assets/js/custom.js
@@ -0,0 +1,26 @@
+function mobileOnlySlider($slidername, $dots, $arrows, $breakpoint) {
+	const slider = $($slidername);
+	const settings = {
+		mobileFirst: true,
+		dots: $dots,
+		arrows: $arrows,
+		responsive: [
+			{
+				breakpoint: $breakpoint,
+				settings: "unslick",
+			},
+		],
+	};
+	$(".slider-template").slick();
+	slider.slick(settings);
+
+	$(window).on("resize", function () {
+		if ($(window).width() > $breakpoint) {
+			return;
+		}
+		if (!slider.hasClass("slick-initialized")) {
+			return slider.slick(settings);
+		}
+	});
+}
+mobileOnlySlider(".slider-template", false, true, 1024);
